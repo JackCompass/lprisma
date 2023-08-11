84fbeff19f8f0f0becc8f4c6476906ee0495c7ae
anuj <anuj.singh@roro.io>
2023-08-11 23:29:27 +0530

    feat: add pets module

---
+++ src/pets/pets.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PetsService } from './pets.service';
+import { PetsResolver } from './pets.resolver';
+import { PrismaService } from 'src/prisma/prisma.service';
+@Module({
+  providers: [PetsService, PetsResolver, PrismaService],
+})
+export class PetsModule {}
